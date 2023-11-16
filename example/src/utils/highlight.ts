b350ff2ece811077d4bed61264b457e456d643e6
Charlie Hadden <charlie@hadden.me>
2023-11-16 17:31:13 +0000

    Run formatter

--- example/src/utils/highlight.ts
+++ example/src/utils/highlight.ts
@@ -1,9 +1,9 @@
-import hljs from 'highlight.js/lib/core';
-import javascript from 'highlight.js/lib/languages/javascript';
-import xml from 'highlight.js/lib/languages/xml';
+import hljs from "highlight.js/lib/core";
+import javascript from "highlight.js/lib/languages/javascript";
+import xml from "highlight.js/lib/languages/xml";
 
-hljs.registerLanguage('javascript', javascript);
-hljs.registerLanguage('xml', xml);
+hljs.registerLanguage("javascript", javascript);
+hljs.registerLanguage("xml", xml);
 
 export function highlight(code: string) {
   return hljs.highlight(code, { language: "jsx" }).value;
