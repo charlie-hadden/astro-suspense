a6430061d0a2d0f057b5cb3cb69b549068f169e5
Charlie Hadden <charlie@hadden.me>
2023-11-16 13:47:19 +0000

    Set up vercel

--- example/astro.config.mjs
+++ example/astro.config.mjs
@@ -1,14 +1,12 @@
 import { defineConfig } from "astro/config";
-import node from "@astrojs/node";
 import tailwind from "@astrojs/tailwind";
 import suspense from "astro-suspense";
 import react from "@astrojs/react";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
-  integrations: [tailwind(), suspense(), react()],
+  adapter: vercel(),
+  integrations: [tailwind(), suspense(), react()]
 });
